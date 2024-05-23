2fbc8b4a95330f9a78770e757f6363a6cb8525f0
Mitchell Merry <mitchellmerry145@gmail.com>
2024-05-23 23:40:41 +1000

    I cracked the code

---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+#![feature(type_alias_impl_trait, const_async_blocks)]
+
+mod zdoom;
+
+use std::error::Error;
+
+use asr::{deep_pointer::DeepPointer, future::next_tick, watcher::Watcher, Address, Process};
+use zdoom::{NameManager, PClass};
+
+asr::async_main!(nightly);
+
+async fn main() {
+    // TODO: Set up some general state and settings.
+
+    asr::print_message("Hello, World!");
+
+    loop {
+        let process = Process::wait_attach("lzdoom.exe").await;
+        process
+            .until_closes(async {
+                asr::print_message("a");
+                let main_exe_addr = process
+                    .get_module_address("lzdoom.exe")
+                    .expect("failed getting lzdoom");
+                asr::print_message("a");
+                let memory = Memory::new(main_exe_addr);
+                let mut watchers = Watchers::default();
+                watchers.update(&process, &memory);
+
+                asr::print_message("a");
+                let name_data = NameManager::new(
+                    watchers
+                        .name_manager_base
+                        .pair
+                        .expect("need a valid thingy")
+                        .current,
+                );
+
+                asr::print_message("a");
+                if let Some(pactor_class) = &watchers.player_actor_class.pair {
+                    let class = PClass::new(pactor_class.current.into());
+                    asr::print_message("a");
+                    class.debug_all_fields(&process, &name_data).expect("aaah");
+                    asr::print_message("a");
+
+                    let name = class.name(&process);
+                    match name {
+                        Ok(name) => {
+                            asr::timer::set_variable("class", &name);
+                        }
+                        Err(error) => {
+                            asr::timer::set_variable("class", &format!("{:?}", error));
+                        }
+                    }
+                } else {
+                    asr::timer::set_variable("class", "unknown");
+                }
+
+                // TODO: Load some initial information from the process.
+                loop {
+                    next_tick().await;
+                }
+            })
+            .await;
+    }
+}
+
+#[derive(Default)]
+struct Watchers {
+    name_manager_base: Watcher<Address>,
+    player_actor_class: Watcher<u64>,
+}
+
+impl Watchers {
+    fn update(&mut self, game: &Process, memory: &Memory) {
+        self.name_manager_base
+            .update(memory.namedata_ptr.deref_offsets(game).ok());
+        self.player_actor_class
+            .update(memory.player_actor_class_ptr.deref(game).ok());
+    }
+}
+
+struct Memory {
+    namedata_ptr: DeepPointer<1>,
+    player_actor_class_ptr: DeepPointer<3>,
+}
+
+impl Memory {
+    fn new(main_exe_addr: Address) -> Memory {
+        Memory {
+            namedata_ptr: DeepPointer::new(main_exe_addr, asr::PointerSize::Bit64, &[0x9F8E10]),
+            player_actor_class_ptr: DeepPointer::new(
+                main_exe_addr,
+                asr::PointerSize::Bit64,
+                &[0x7043C0, 0x0, 0x8],
+            ),
+        }
+    }
+}
---
+++ src/zdoom.rs
@@ -0,0 +1,234 @@
+use std::{marker::PhantomData, mem, ops::Add, process};
+
+use asr::{print_message, string::ArrayCString, Address, Error, Process};
+use bytemuck::CheckedBitPattern;
+
+const NAME_ENTRY_SIZE: u64 = 0x10;
+const PFIELD_NAME: u64 = 0x28;
+const PFIELD_OFFSET: u64 = 0x38;
+const PFIELD_TYPE: u64 = 0x40;
+
+struct TArray<T: CheckedBitPattern> {
+    _phantom: PhantomData<T>,
+    address: Address,
+}
+
+impl<T: CheckedBitPattern> TArray<T> {
+    pub fn new(address: Address) -> TArray<T> {
+        TArray {
+            _phantom: PhantomData,
+            address,
+        }
+    }
+
+    pub fn into_iter<'a>(&self, process: &'a Process) -> Result<TArrayIterator<'a, T>, Error> {
+        let size =
+            process.read_pointer_path(self.address, asr::PointerSize::Bit64, &[0x8 as u64])?;
+        Ok(TArrayIterator::<T> {
+            _phantom: PhantomData,
+            process,
+            address: self.address,
+            size,
+            index: 0,
+        })
+    }
+}
+
+struct TArrayIterator<'a, T: CheckedBitPattern> {
+    _phantom: PhantomData<T>,
+    process: &'a Process,
+    address: Address,
+    size: u32,
+    index: u32,
+}
+
+impl<'a, T: CheckedBitPattern> Iterator for TArrayIterator<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.size {
+            return None;
+        }
+
+        // asr::print_message(&format!("{}/{}", self.index, self.size));
+
+        let offset = (std::mem::size_of::<T>() as u32 * self.index) as u64;
+        // let offset = (0x10 * self.index) as u64;
+        // asr::print_message(&format!("offset: {offset:X}"));
+
+        // let item = self
+        //     .process
+        //     .read_pointer_path::<u64>(self.address, asr::PointerSize::Bit64, &[0x0])
+        //     .ok()
+        //     .unwrap();
+        // asr::print_message(&format!("addr: {item:X}"));
+
+        let item = self
+            .process
+            .read_pointer_path(self.address, asr::PointerSize::Bit64, &[0x0, offset])
+            .ok();
+
+        self.index = self.index + 1;
+
+        return item;
+    }
+}
+
+pub struct NameManager {
+    address: Address,
+}
+
+impl NameManager {
+    pub fn new(address: Address) -> NameManager {
+        NameManager { address }
+    }
+
+    pub fn get_chars(&self, process: &Process, index: u32) -> Result<String, Error> {
+        // asr::print_message(&format!("using thing: {} {index}", self.address.add(0x8)));
+
+        let read = process.read_pointer_path::<ArrayCString<128>>(
+            self.address,
+            asr::PointerSize::Bit64,
+            &[0x8, index as u64 * NAME_ENTRY_SIZE, 0x0],
+        );
+
+        // let a = process.read_pointer_path::<u64>(
+        //     self.address,
+        //     asr::PointerSize::Bit64,
+        //     &[0x8, index as u64 * NAME_ENTRY_SIZE],
+        // )?;
+        // asr::print_message(&format!("check: {:X}", a));
+
+        // let a = process.read
+
+        let read = match read {
+            Ok(cstr) => cstr,
+            Err(e) => {
+                asr::print_message(&format!("what? a{e:?}"));
+                panic!("waaaah..");
+            }
+        };
+
+        match read.validate_utf8() {
+            Ok(s) => Ok(s.to_owned()),
+            Err(e) => {
+                asr::print_message(&format!("what? {e}"));
+                panic!("waaaah..");
+            }
+        }
+    }
+}
+
+pub struct PClass {
+    address: Address,
+}
+
+impl PClass {
+    pub fn new(addr: Address) -> PClass {
+        PClass { address: addr }
+    }
+
+    pub fn name(&self, process: &Process) -> Result<String, Error> {
+        let vm_type = PType {
+            address: process
+                .read_pointer_path::<u64>(self.address, asr::PointerSize::Bit64, &[0x88])?
+                .into(),
+        };
+
+        return vm_type.name(process);
+    }
+
+    pub fn debug_all_fields(
+        &self,
+        process: &Process,
+        name_data: &NameManager,
+    ) -> Result<(), Error> {
+        let parent_class: Address = process
+            .read_pointer_path::<u64>(self.address, asr::PointerSize::Bit64, &[0x0])?
+            .into();
+
+        if parent_class != Address::NULL {
+            let parent_class = PClass::new(parent_class);
+            parent_class.debug_all_fields(process, name_data)?;
+        }
+
+        asr::print_message(&format!("{} fields:", self.name(process)?));
+
+        let fields_addr = self.address.add(0x78);
+        // process.read_pointer_path::<u64>(self.address, asr::PointerSize::Bit64, &[0x78])?;
+        asr::print_message(&format!("fields_addr {:?}", fields_addr));
+        let field_addrs = TArray::<u64>::new(fields_addr.into());
+        // asr::print_message("a");
+
+        for field_addr in field_addrs.into_iter(process)? {
+            // asr::print_message(&format!("field_addr {:X}", field_addr));
+            let field = PField::new(field_addr.into());
+            // asr::print_message("a");
+            asr::print_message(&format!(
+                "  => 0x{:X}  {}: {} [{:X}]",
+                field.offset(process)?,
+                field.name(process, name_data)?,
+                field.ptype(process)?.name(process)?,
+                field_addr
+            ));
+        }
+
+        Ok(())
+    }
+}
+
+struct PField {
+    address: Address,
+}
+
+impl PField {
+    pub fn new(address: Address) -> Self {
+        PField { address }
+    }
+
+    pub fn name(&self, process: &Process, name_data: &NameManager) -> Result<String, Error> {
+        // asr::print_message(&format!("what {}", self.address));
+        let name_index: u32 =
+            process.read_pointer_path(self.address, asr::PointerSize::Bit64, &[PFIELD_NAME])?;
+        // asr::print_message("a");
+
+        name_data.get_chars(process, name_index)
+    }
+
+    pub fn offset(&self, process: &Process) -> Result<u32, Error> {
+        process.read_pointer_path(self.address, asr::PointerSize::Bit64, &[PFIELD_OFFSET])
+    }
+
+    pub fn ptype(&self, process: &Process) -> Result<PType, Error> {
+        // PFIELD_TYPE
+        let ptype: Address = process
+            .read_pointer_path::<u64>(self.address, asr::PointerSize::Bit64, &[PFIELD_TYPE])?
+            .into();
+        Ok(PType::new(ptype))
+    }
+}
+
+struct PType {
+    address: Address,
+}
+
+impl PType {
+    fn new(address: Address) -> PType {
+        PType { address }
+    }
+
+    fn name(&self, process: &Process) -> Result<String, Error> {
+        let c_str = process.read_pointer_path::<ArrayCString<128>>(
+            self.address,
+            asr::PointerSize::Bit64,
+            &[0x48, 0x0],
+        );
+
+        let b = c_str?
+            .validate_utf8()
+            .expect("name should always be utf-8")
+            .to_owned();
+
+        return Ok(b);
+    }
+}
